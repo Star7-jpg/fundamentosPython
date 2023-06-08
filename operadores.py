ce24f7b520f2c087a387c594c16904ced464eaa9
Star7-jpg <133389865+Star7-jpg@users.noreply.github.com>
2023-06-08 14:44:58 -0600

    03 Operadores

---
+++ operadores.py
@@ -0,0 +1,19 @@
+#Operadores Matemáticos
+# + - * %
+# / Divsión flotante
+#// División entera
+#** Potencia
+
+print(24 + 62)
+print(50 - 32)
+print(35 * 2)
+print(10 % 3)
+print(10 / 3)
+print(10 // 3)
+print(3 ** 3)
+
+#Operadores string
+# + para concatenar
+# * replicar un string
+print("Hola a " + "todos")
+print("DSM 3A " * 4)
