0be3bb3a9b68a5e0bd24ffb7541fc46463a8295e
Star7-jpg <133389865+Star7-jpg@users.noreply.github.com>
2023-06-08 14:32:53 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,18 @@
+#Python es NO TIPADO
+name = "Marcos"
+age = 23
+is_single = True
+
+print(name)
+print(age)
+print(is_single)
+
+"""
+Hola soy Francisco, tengo 39 años y soltero = True
+"""
+
+#Utilizando format String
+print("Hola soy {}, tengo {} años y soltero = {}".format(name, age, is_single))
+
+print(f"Hola soy {name}, tengo {age} años y soltero = {is_single}")
+
