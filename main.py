9b65236360bb12ca3876536dfae675e22b2959e7
Star7-jpg <133389865+Star7-jpg@users.noreply.github.com>
2023-06-08 14:14:48 -0600

    01 Iniciando con Python

---
+++ main.py
@@ -0,0 +1,12 @@
+#function print ()
+print("Este es un String","Hola Mundo")
+print("Este es un entero",35)   
+print("Este es un float",3.14)
+print("Este es un booleano",True)
+
+
+#function type nos permite saber tipos de dato
+print(type(True))
+print(type(3.14))
+print(type(3))
+print(type(""))
