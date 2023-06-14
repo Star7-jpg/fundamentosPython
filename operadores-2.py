6dfa09812be92692ab820eec965b8c5d92039635
Star7-jpg <133389865+Star7-jpg@users.noreply.github.com>
2023-06-14 12:36:34 -0600

    05 Operadores Logicos y booleanos

---
+++ operadores-2.py
@@ -0,0 +1,29 @@
+#Operadores Booleanos
+print("Operadores Booleanos")
+# < <=
+print(10 < 10)
+print (10 <= 10)
+
+#> >=
+print(10 > 10)
+print(10 >= 10)
+
+#==
+print (10 == 10)
+
+# !=
+print(10 != 10)
+
+# Operadores lógicos
+print("Operadores lógicos")
+
+# (True if all true)
+print(True and True)
+print(True and False)
+
+#(True if at least one True)
+print( False or False)
+print( False or True)
+#not True
+print(not True)
+print(not False)
